8fc41600bb21d5991276869b47f6eb70056a9883
Antônio Junior <acsjunior1@gmail.com>
2020-06-25 19:24:33 -0300

    subscription test

--- __tests__/unit/resolvers/poll.subscriptions.test.js
+++ __tests__/unit/resolvers/poll.subscriptions.test.js
@@ -1,7 +1,11 @@
-import { gql } from 'apollo-server-express';
+import { gql, PubSub } from 'apollo-server-express';
+import { graphql, subscribe, parse } from 'graphql';
 
+import schema from '../../../src/graphql/schema';
 import config from '../../config-sequelize';
-import { tester } from '../utils/tester';
+import { createFakePoll } from '../utils/pollBuilder';
+import { tester, context } from '../utils/tester';
+import { getFakeUser } from '../utils/userBuilder';
 
 config();
 
@@ -17,5 +21,61 @@ describe('Test Poll Subscriptions', () => {
     `;
 
     tester.test(true, pollSubscription);
+  });
+
+  test('should subscribe to invites', async () => {
+    const poll = await createFakePoll();
+    const userToInvite = getFakeUser();
+
+    const newContext = {
+      ...context,
+      pubSub: new PubSub(),
+      userId: poll.userId
+    };
+
+    const mutation = `
+      mutation POLL_MUTATION($invites: [InputInvite]!) {
+        addInvites(invites: $invites)
+      }
+    `;
+
+    const variablesMutation = {
+      invites: [{ pollsetId: poll.id, email: userToInvite.email }]
+    };
+
+    const subscription = `
+      subscription {
+        invited {
+          id
+          title
+        }
+      }
+    `;
+
+    const triggerSubscription = graphql(
+      schema,
+      mutation,
+      undefined,
+      newContext,
+      variablesMutation
+    );
+
+    const result = await subscribe(
+      schema,
+      parse(subscription),
+      triggerSubscription,
+      newContext
+    );
+
+    const resultData = await result.next();
+
+    const expected = {
+      invited: {
+        id: poll.id.toString(),
+        title: poll.title
+      }
+    };
+
+    expect(resultData.value.data).toEqual(expected);
   });
 });
